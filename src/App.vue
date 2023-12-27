5092623ded6f9d43d527d851fabde02b649282c2
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-12-27 13:36:52 +0100

    New project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <div class="mt-0"></div>
+  </header>
+
+  <!-- 
+
+      Day
+  DD
+
+  Month
+  MM
+
+  Year
+  YYYY
+
+  -- years
+  -- months 
+  -- days
+   -->
+  <main>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
